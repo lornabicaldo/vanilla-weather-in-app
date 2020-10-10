d99d469d7258741ca0fb3f7fc868f979326947b0
lornabicaldo <69823972+lornabicaldo@users.noreply.github.com>
2020-10-10 16:21:23 +0200

    Updated the current time.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,32 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -6,6 +35,7 @@ function displayTemperature(response) {
   let speedElement = document.querySelector("#speed");
   let pressureElement = document.querySelector("#pressure");
   let feelsLikeElement = document.querySelector("#feels-like");
+  let dateElement = document.querySelector("#date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -14,6 +44,7 @@ function displayTemperature(response) {
   speedElement.innerHTML = Math.round(response.data.wind.speed);
   pressureElement.innerHTML = response.data.main.pressure;
   feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "8faad2c6827a8ece72c2320ecccefbdb";
